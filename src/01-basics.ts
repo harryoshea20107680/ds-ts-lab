f44954ad75ca5667e445433379ad26c011045cbf
Harry OShea <20107680@setu.ie>
2026-09-14 12:27:57 +0100

    Added type-safety to friend data.

---
+++ src/01-basics.ts
@@ -0,0 +1,56 @@
+//interface for friend added:
+interface Friend {
+    name: string;
+    phone: string;
+    age: number
+}
+
+
+const friend1 = {
+  name: "Michael Brick",
+  phone: "087-12345",
+  age: 25,
+};
+
+const friend2 = {
+  name: "Martin Youngfella",
+  phone: "086--12345",
+  age: 31,
+};
+
+const friends = [friend1, friend2];
+console.log(friends[1]);
+
+//   -------------------
+const colleague1 = {
+  name: "Ralph Graham",
+  department: "Engineering",
+  contact: {
+    email: "rgraham@company.com",
+    extension: 121,
+  },
+};
+
+const colleague2 = {
+  name: "Patti Burke",
+  department: "Finance",
+  contact: {
+    email: "pburke@company.com",
+    extension: 132,
+  },
+};
+
+const colleague3 = {
+  name: "Dean Sullivan",
+  department: "HR",
+  contact: {
+    email: "dos@company.com",
+    extension: 125,
+  },
+};
+const colleagues = {
+  current: [colleague1, colleague2, colleague3],
+  former: [],
+};
+
+console.log(colleagues.current[0]);

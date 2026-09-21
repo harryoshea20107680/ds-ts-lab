a45ff87c3bcc5c079cd0b2fed70a47791d3c0942
Harry OShea <20107680@setu.ie>
2026-09-21 12:32:03 +0100

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague, EmailContact } from './myTypes'
 //import objects for testing:
 import {friends, colleagues} from './01-basics'
 
@@ -11,17 +11,17 @@ console.log(older(friends[0]))
 
 
 // Find the colleague with the highest extension number.
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) { // Inferred retun type
   const result = cs.sort(
     (c1, c2) => c1.contact.extension - c2.contact.extension
   );
   return result[cs.length - 1];
 }
-console.log(highestExtension(colleagues.current));
+
 
 
 // adds a colleague to an array, and setting their extensins number to the highest extension, plus 1:
-function addColleague(cs: Colleague[], name: string, department: string, email: string): String {
+function addColleague(cs: Colleague[], name: string, department: string, email: string){ //Inferred string return
   cs[cs.length + 1] = {
   name: name,
   department: department,
@@ -34,7 +34,36 @@ function addColleague(cs: Colleague[], name: string, department: string, email: 
 }
 
 
-//to test add colleague:
+//to test addCcolleague:
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+
+
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+
+//Searches an array of friends for those that satisfy a criterion. 
+//The criterion is specified as a callback.
+function findFriends(
+  friends: Friend[],
+  criterion: (friend: Friend) => boolean
+): string[] {
+  const foundFriends = friends.filter(criterion);
+  return foundFriends.map((friend) => friend.name)
+}
+
+
+console.log('Friends starting with Ma: ' + findFriends(friends, (friend) => friend.name.startsWith('Ma')));
+console.log('friends over 35: ' + findFriends(friends, (friend) => friend.age < 35));
 

6ddb6f587f3c69ecb747ad891d31da0ac4c7e12f
Harry OShea <20107680@setu.ie>
2026-09-22 10:50:24 +0100

    Create cafe.ts and menuTypes.d.ts make preliminary JS->TS edits

---
+++ src/menuTypes.d.ts
@@ -0,0 +1,32 @@
+export type Course = "starter" | "main" | "dessert" ;
+
+export type OrderLine = MenuItem | ComboDeal ;
+
+export type KitchenTicket = Readonly<Pick<MenuItem, "name" | "course" > >
+
+export interface MenuItem {
+    id: number;
+    name: string;
+    course: Course;
+    price: number;
+    nutrition: {
+        calories: number;
+        allergens: string[]
+    }
+    discountPercent?: number;
+    availableFrom?: Date;
+}
+
+export type MenuItemPartial = Partial<MenuItem>
+
+export interface ComboDeal {
+    id: number;
+    name: string;
+    items: string[];
+    price: number
+}
+
+export type AllergyCard = Omit<MenuItem, "nutrition"> & {
+    warning: string;
+}
+

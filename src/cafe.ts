8951414df9b11396b5d833ef59db8ef5d421a618
Harry OShea <20107680@setu.ie>
2026-09-22 12:17:50 +0100

    added correct object path i.nutritioon.calories to firstMatch console test

--- src/cafe.ts
+++ src/cafe.ts
@@ -166,4 +166,4 @@ console.log(allergyCard(brownie));
 //     your types are in place, fix each one and note it in your commit message.
 console.log(describe(lunchCombo));
 console.log(updateItem(soup, { price: 7.00 }));
-console.log(firstMatch(menu, (i) => i.calories < 300));
+console.log(firstMatch(menu, (i) => i.nutrition.calories < 300));

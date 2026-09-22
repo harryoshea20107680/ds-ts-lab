f493bd37176c932ea584b43f3f9878f82056c45b
Harry OShea <20107680@setu.ie>
2026-09-22 12:17:02 +0100

    Log to pass in a number instead of a string to updateItem

--- src/cafe.ts
+++ src/cafe.ts
@@ -165,5 +165,5 @@ console.log(allergyCard(brownie));
 // TS: Three more lines below are bugs that only the compiler can see. Once
 //     your types are in place, fix each one and note it in your commit message.
 console.log(describe(lunchCombo));
-console.log(updateItem(soup, { price: "7.00" }));
+console.log(updateItem(soup, { price: 7.00 }));
 console.log(firstMatch(menu, (i) => i.calories < 300));
